8c71be98543d708b83e7d5a5a171e803dd6dae7f
akhmadst1 <13521164@mahasiswa.itb.ac.id>
2023-11-29 22:53:28 +0700

    final push

--- src/knn_sklearn.py
+++ src/knn_sklearn.py
@@ -17,8 +17,8 @@ def load_data(file_path):
     return features, labels, header[:-1]
 
 
-train_file_path = './data/data_train.csv'
-validation_file_path = './data/data_validation.csv'
+train_file_path = '../data/data_train.csv'
+validation_file_path = '../data/data_validation.csv'
 
 x_train, y_train, _ = load_data(train_file_path)
 x_validation, y_validation, _ = load_data(validation_file_path)
@@ -28,14 +28,15 @@ knn21 = KNeighborsClassifier(n_neighbors=21)
 
 # Train the model
 knnmodel = knn21.fit(x_train, y_train)
-with open('./models/knn_sklearn_model.pkl', 'wb') as model_file:
+with open('../models/knn_sklearn_model.pkl', 'wb') as model_file:
     pickle.dump((knnmodel), model_file)
 
 # Making predictions by using pred() function
-with open('./models/knn_sklearn_model.pkl', 'rb') as model_file:
+with open('../models/knn_sklearn_model.pkl', 'rb') as model_file:
     loaded_model = pickle.load(model_file)
 
 y_pred_21 = loaded_model.predict(x_validation)
-
+print("Predictions:")
+print(y_pred_21)
 print("Accuracy of KNN with k=21 is",
       accuracy_score(y_validation, y_pred_21)*100)
